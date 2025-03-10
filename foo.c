bebae31fb984ed7e3b72dc803f4a43c21f2f4403
Anderson Julião <antonioaaj@al.insper.edu.br>
2025-03-10 03:00:55 -0300

    try 01

---
+++ foo.c
@@ -0,0 +1,7 @@
+int var; 
+
+int bar(int b) {
+    if (b > 2) 
+        return 0;
+    return 1;
+}
--- foo.h
+++ foo.h
@@ -1,7 +1,9 @@
 // foo.h
 
-int foo(int b) {
-  if (b > 2)
-    return 0;
-  return 1;
-}
+#ifndef FOO_H
+#define FOO_H
+#include "foo.h"
+
+int foo(int b)
+
+#endif
